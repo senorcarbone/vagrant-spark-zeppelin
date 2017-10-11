280e0093337222b4d9a21e669dc2e90cc12c0e70
Paris Carbone <parisc@kth.se>
2017-10-11 12:20:15 +0200

    oracle jdk and script changes for zeppelics v2

--- install/install-01-oracle-java.sh
+++ install/install-01-oracle-java.sh
@@ -1,23 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# JAVA
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/jre/
-export PATH=$PATH:$JAVA_HOME/bin
-
-echo
-echo
-echo "Installing Java"
-
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-apt-get update
-
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
-
-export DEBIAN_FRONTEND=noninteractive 
-apt-get install -y --force-yes oracle-java7-installer oracle-java7-set-default
-
-java -version
+if which java >/dev/null; then
+   	echo "skip java 8 installation"
+else
+	echo "java 8 installation"
+	apt-get install --yes python-software-properties
+	add-apt-repository ppa:webupd8team/java
+	apt-get update -qq
+	echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | /usr/bin/debconf-set-selections
+	apt-get install --yes oracle-java8-installer
+	yes "" | apt-get -f install
+fi
